1be1e99eee8adb8a9cafd243ca23cdd1f65550a7
maxlestage <maxlestage@icloud.com>
2022-02-03 15:30:35 +0100

    Done ✅

--- src/App.js
+++ src/App.js
@@ -6,12 +6,29 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const addPerson = (event) => {
+    event.preventDefault();
+    console.log("Button Clicked", event.target);
+    const personObject = {
+      id: persons.length + 1,
+      name: newName,
+    };
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+
+  const handlePersonChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  console.log(persons);
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handlePersonChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -19,6 +36,11 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>debug: {newName}</div>
+      <ul>
+        {persons.map((person) => {
+          return <li key={person.name + person.id}>{person.name}</li>;
+        })}
+      </ul>
     </div>
   );
 };
