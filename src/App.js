de5ba48d8f319f0a31b8e0ccbea580301944f924
maxlestage <maxlestage@icloud.com>
2022-02-04 14:49:24 +0100

    2.9* : Le répertoire Step4

--- src/App.js
+++ src/App.js
@@ -3,20 +3,31 @@ import "./App.css";
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchPerson, setSearchPerson] = useState("");
+
+  const allPersons = persons.map((person) => {
+    return person.name;
+  });
 
   const addPerson = (event) => {
     event.preventDefault();
     const personObject = {
       name: newName,
+      number: newNumber,
     };
 
-    console.log(personObject.name);
-
-    const allPersons = persons.map((person) => {
-      return person.name;
-    });
+    // const allPersons = persons.map((person) => {
+    //   return person.name;
+    // });
 
     allPersons.forEach((name) => {
       if (name === newName) {
@@ -24,30 +35,65 @@ const App = () => {
       } else {
         setPersons(persons.concat(personObject));
         setNewName("");
+        setNewNumber("");
       }
     });
   };
+
+  console.log(allPersons);
 
   const handlePersonChange = (event) => {
     setNewName(event.target.value);
   };
 
+  const handleNumberchange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const search = (e) => setSearchPerson(e.target.value);
+
+  console.log(searchPerson);
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        <p>
+          filter shown with : <input value={searchPerson} onChange={search} />
+        </p>
+        <div>
+          {allPersons.filter((name) => name.includes(searchPerson)) ? (
+            <ul>
+              {allPersons.map((name) => (
+                <li key={name}>{name}</li>
+              ))}
+            </ul>
+          ) : (
+            <p>Nothing</p>
+          )}
+        </div>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handlePersonChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberchange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>debug: {newName}</div>
+      {/* <div>debug Name: {newName}</div>
+      <div>debug Number: {newNumber}</div> */}
       <ul>
         {persons.map((person) => {
-          return <li key={person.name}>{person.name}</li>;
+          return (
+            <li key={person.name}>
+              {person.name}: {person.number}{" "}
+            </li>
+          );
         })}
       </ul>
     </div>
